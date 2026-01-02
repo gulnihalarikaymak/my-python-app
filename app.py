3a87658c34ccde244c04b09ac1b602d86f71a3a4
gulnihalarikaymak <arikaymakg@gmail.com>
2026-01-03 00:40:18 +0300

    Update Message 2

--- app.py
+++ app.py
@@ -4,16 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return """
-    <h1>GUL NİHAL ARİKAYMAK'S Flask CI/CD Demo 🚀</h1>
-    <p>This application is deployed on an Azure Virtual Machine.</p>
-    <ul>
-      <li>🐳 Containerized with Docker</li>
-      <li>⚙️ Managed and deployed using Coolify</li>
-      <li>🔁 Connected to GitHub for automatic CI/CD</li>
-    </ul>
-    <p>Every push to the GitHub repository triggers an automatic redeploy.</p>
-    """
+    return "This Flask app is deployed on Azure using Docker & Coolify with GitHub-based CI/CD 🚀"
+
 
 
 if __name__ == '__main__':
